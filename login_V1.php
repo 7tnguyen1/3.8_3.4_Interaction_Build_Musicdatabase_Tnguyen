4f1b9a269bf555b11e5e5ef5c4eedd4bb3e7469b
7tnguyen1 <86210235+7tnguyen1@users.noreply.github.com>
2021-08-06 09:30:27 +1200

    Update login_V1.php

--- login_V1.php
+++ login_V1.php
@@ -1,29 +1,31 @@
 <!DOCTYPE html>
 <html lang = "en">
 	<head>
-		<title> LoginPage</title>
+		<title>Login</title>
 		<meta chrset="utf-8"/>
 		<meta name="Keywords" content = "html5, layout, CSS Grid System"/>
 		<meta name = "Author" content = "Tony Nguyen"/>
 		<meta name = "Description" content = "CSS Grid System Layout Tutorial"/>
 		<meta name = "viewport" content = "width=device-width. initial-scale=1"/>
 		
-		<link rel ="stylesheet" href = "style.css"> 
+		<link rel ="stylesheet" href = "style7.css"> 
 		
 	</head>
 <body>
 	<div class ="grid-container">
 		<div class="navigation">
-			<nav>
-				<ul>
-					<li><a href = "index.html">Home</a></li>
-					<li><a href = "index.html">Music Play</a></li>
-					<li><a href = "index.html">Contact</a></li>
-					<li><a href = "01_login_real.php">Login</a></li>
-					<li><a href = "01_login_real.php">SignUp</a></li>
-				</ul>
-			</nav>
+			<div class="logo">
+				<a href = "01_login_real.php"><img src = "images/logo8.png" height = "100"></a>
+			</div>
+			<?php
+				//Pulls the links from the nav.php page and places them in the navigation div
+				require 'nav_login.css'; //'require' is 100% needed for the site to run 
+			?>
 		</div>
+		<div class="img_2">
+			<img src = "images/img2.jpg">
+		</div>
+		<div class = "footer">&copy; Copyright Tony Nguyen 2021</div>
 	</div>
 </body>
 </html>
