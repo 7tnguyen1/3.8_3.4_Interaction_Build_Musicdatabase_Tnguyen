a3dd006060e3676a75ecb6618abc0349ece8e7cb
7tnguyen1 <86210235+7tnguyen1@users.noreply.github.com>
2021-08-06 09:34:03 +1200

    Update login_V1.php

--- login_V1.php
+++ login_V1.php
@@ -1,3 +1,40 @@
+<?php
+ob_start();
+session_start();
+	
+	$error = NULL;
+
+	if($_SERVER["REQUEST_METHOD"] == "POST") {
+		require 'musicdatabase_database_mysqli.php';
+		//username and password sent from form
+		$myusername = mysqli_real_escape_string($conn, $_POST['username']);
+		$mypassword = mysqli_real_escape_string($conn, $_POST['password']);
+		
+		$query1 = "SELECT User FROM Admin WHERE User = '".$myusername."' and Password = '".$mypassword."'";
+		$query2 = "SELECT User FROM User WHERE User = '".$myusername."' and Password = '".$mypassword."'";
+
+		$result1 = mysqli_query($conn, $query1);
+		$result2 = mysqli_query($conn, $query2);
+
+		$count1 = mysqli_num_rows($result1);
+		$count2 = mysqli_num_rows($result2);
+
+		// If result matched $myusername and $mypassword, table row must be 1 row
+		if($count1 == 1 and $myusername !== '' and $mypassword !== '') {
+			$_SESSION['login_user'] = $myusername;
+			$_SESSION['user_type'] = 'user';
+			header("location: homepage_for_admin.php");
+		} else if($count2 == 1 and $myusername !== '' and $mypassword !== '') {
+			$_SESSION['login_user'] = $myusername;
+			$_SESSION['user_type'] = 'admin';
+			header("location: homepage.php");
+		} else if($myusername !== '' and $mypassword !== '') {
+			$error = "Your username or password is incorrect";
+		}
+	}
+ob_end_flush();
+?>
+
 <!DOCTYPE html>
 <html lang = "en">
 	<head>
@@ -21,6 +58,15 @@
 				//Pulls the links from the nav.php page and places them in the navigation div
 				require 'nav_login.css'; //'require' is 100% needed for the site to run 
 			?>
+		</div>
+		<div class="content"><!-- Holds the main page content -->
+			<p>Welcome to Music MP3</p>
+			<h3><form method = "post" id = "01_login">
+			<input id="us" type = "text" name = "username" placeholder="User name"/><br />
+			<input id="pa" type = 'password' name = 'password' placeholder="Password"/><br />
+			<input  id="login" type = "submit" value = "login"/><br />
+			<a href = "Update_user.php">Forgot your password?</a>
+			</form></h3>
 		</div>
 		<div class="img_2">
 			<img src = "images/img2.jpg">
