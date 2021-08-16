cb50f3305f89a51b9cecde5cde123f01393c828f
7tnguyen1 <86210235+7tnguyen1@users.noreply.github.com>
2021-08-16 12:06:17 +1200

    Update login.php

--- login.php
+++ login.php
@@ -1,40 +1,38 @@
 <!DOCTYPE html>
 <html lang = "en">
 	<head>
-		<title>Layout1Login</title>
+		<title>Login</title>
 		<meta chrset="utf-8"/>
 		<meta name="Keywords" content = "html5, layout, CSS Grid System"/>
 		<meta name = "Author" content = "Tony Nguyen"/>
 		<meta name = "Description" content = "CSS Grid System Layout Tutorial"/>
 		<meta name = "viewport" content = "width=device-width. initial-scale=1"/>
 		
-		<link rel ="stylesheet" href = "style5.css"> 
+		<link rel ="stylesheet" href = "login.css"> 
 		
 	</head>
 <body>
 	<div class ="grid-container">
-		<div class="img_1">
-			<img src = "images/img.png">
-		</div>
 		<div class="navigation">
-			<nav>
-				<ul>
-					<li><a href = "index.html">Home</a></li>
-					<li><a href = "index.html">Music Play</a></li>
-					<li><a href = "index.html">Contact</a></li>
-					<li><a href = "01_login_real.php">Account</a></li>
-				</ul>
-			</nav>
+			<div class="logo">
+				<a href = "#"><img src = "images/logo8.png" height = "100"></a>
+			</div>
+			<?php
+				//Pulls the links from the nav.php page and places them in the navigation div
+				require 'nav_login.php'; //'require' is 100% needed for the site to run 
+			?>
 		</div>
 		<div class="content"><!-- Holds the main page content -->
-			<div class="section3">
-				<p>Welcome to Music MP3</p>
-				<h3><form method = "post" id = "01_login">
-				<input id="us" type = "text" name = "username" placeholder="User name"/><br />
-				<input id="pa" type = 'password' name = 'password' placeholder="Password"/><br />
-				<input  id="login" type = "submit" value = "login"/><br />
-				</form></h3>
-			</div>
+			<p>Welcome to Music MP3</p>
+			<h3><form method = "post" id = "01_login">
+			<input id="us" type = "text" name = "username" placeholder="User name"/><br />
+			<input id="pa" type = 'password' name = 'password' placeholder="Password"/><br />
+			<input  id="login" type = "submit" value = "login"/><br />
+			<p class = "error" id="er"><?php echo $error; ?></p>
+			</form></h3>
+		</div>
+		<div class="img_2">
+			<img src = "images/img2.jpg">
 		</div>
 		<div class = "footer">&copy; Copyright Tony Nguyen 2021</div>
 	</div>
