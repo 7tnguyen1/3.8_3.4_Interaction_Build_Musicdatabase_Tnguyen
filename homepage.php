cb61215f28803b352bedddc13fed131fc5e59c2b
7tnguyen1 <86210235+7tnguyen1@users.noreply.github.com>
2021-08-30 12:25:10 +1200

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -1,73 +1,112 @@
+<?php
+		// Deliver to login page if access to this page with out login
+		session_start();
+		if(!isset($_SESSION['login_user'])){
+				header("location:login.php");
+				}
+		else{
+			$User = $_SESSION['login_user'];
+		}
+?>
+
 <!DOCTYPE html>
 <html lang = "en">
 	<head>
-		<title> Homepage</title>
+		<title>Homepage</title>
 		<meta chrset="utf-8"/>
 		<meta name="Keywords" content = "html5"/>
 		<meta name = "Author" content = "Tony Nguyen"/>
 		<meta name = "Description" content = "Music database"/>
 		<meta name = "viewport" content = "width=device-width. initial-scale=1"/>
 		<!--Connected to css -->
-		<link rel ="stylesheet" href = "style5.css"> 
+		<link rel ="stylesheet" href = "style1.css">
 		
 	</head>
 <body>
+	<!--Set for grid system for the page -->
 	<div class ="grid-container">
+		<!-- The div for nav and in the div it contain the logo and a function to called the nav -->
 		<div class="navigation">
+			<!-- Set a div that contain logo for the music database -->
 			<div class="logo">
 				<a href = "homepage.php"><img src = "images/logo8.png" height = "100"></a>
 			</div>
-			<nav>
-				<ul>
-					<li><a href = "homepage.php">Home</a></li>
-					<li><a href = "musicplayer.php">Music Play</a>
-						<ul>
-							<li><a href= "Query1.php">Query1</a></li>
-							<li><a href= "Query2.php">Query2</a></li>
-						</ul>
-					</li>
-					<li><a href = "login.php">Contact</a></li>
-					<li><a href = "login.php">LoginOut</a></li>
-					<li><a href = "signup.php">SignUp</a></li>
-				</ul>
-			</nav>
+			<?php
+				//Pulls the links from the nav_normal.php page and places them in the navigation div
+				require 'nav_normal.php'; //'require' is 100% needed for the site to run 
+			?>
 		</div>
+		<!-- Div that holds the banner -->
 		<div class="img_1">
 			<img src = "images/banner.jpg">
 		</div>
 		<div class="content_1"><!-- Holds the main page content -->
 			<h1>Welcome to MUSIC MP3</h1>
 			<p>MUSIC MP3 is a free music player application. With a high-quality music store, you will have a great music experience.</p>
-			<p>Digital music service with millions of high quality copyrighted songs , helping you listen to music, download music, upload and sync my music store on multiple devices.</p>
+			<p >Digital music service with millions of high quality copyrighted songs , helping you listen to music, download music, upload and sync my music store on multiple devices.</p>
 		</div>
+		<!-- Holds the Imges which all be the link to other special playlist-->
 		<div class="content_4">
 			<Space></Space>
 			<div class="images1">
-				<a href = "#"><img src = "images/img.png"></a>
+				<a href = "Query8.php"><img src = "images/indie.jpg" class="image1"></a>
+				<!--Div for overlay techniques so that when the user hover it hide the images and display text -->
+				<div class="overlay">
+    				<div class="text"><a href = "Query8.php">Indie</a></div>
+  				</div>
 			</div>
 			<div class="images2">
-				<a href = "#"><img src = "images/img.png"></a>
+				<a href = "Query7.php"><img src = "images/R&B.jpg" class="image2"></a>
+				<!--Div for overlay techniques so that when the user hover it hide the images and display text -->
+				<div class="overlay">
+    				<div class="text"><a href = "Query7.php">R and B</a></div>
+  				</div>
 			</div>
 			<div class="images3">
-				<a href = "#"><img src = "images/img.png"></a>
+				<a href = "Query6.php"><img src = "images/pop.jpg" class="image3"></a>
+				<!--Div for overlay techniques so that when the user hover it hide the images and display text -->
+				<div class="overlay">
+    				<div class="text"><a href = "Query6.php">Pop</a></div>
+  				</div>
 			</div>
 			<div class="images4">
-				<a href = "#"><img src = "images/img.png"></a>
+				<a href = "Query10.php"><img src = "images/rock.png" class="image4"></a>
+				<!--Div for overlay techniques so that when the user hover it hide the images and display text -->
+				<div class="overlay">
+    				<div class="text"><a href = "Query10.php">Rock</a></div>
+  				</div>
 			</div>
 			<Space></Space>
 			<div class="images5">
-				<a href = "#"><img src = "images/img.png"></a>
+				<a href = "Query9.php"><img src = "images/easylisten.jpg" class="image5"></a>
+				<!--Div for overlay techniques so that when the user hover it hide the images and display text -->
+				<div class="overlay">
+    				<div class="text"><a href = "Query9.php">Easy Listen</a></div>
+  				</div>
 			</div>
 			<div class="images6">
-				<a href = "#"><img src = "images/img.png"></a>
+				<a href = "Query3.php"><img src = "images/country.png" class="image6"></a>
+				<!--Div for overlay techniques so that when the user hover it hide the images and display text -->
+				<div class="overlay">
+    				<div class="text"><a href = "Query3.php">Country</a></div>
+  				</div>
 			</div>
 			<div class="images7">
-				<a href = "#"><img src = "images/img.png"></a>
+				<a href = "Query4.php"><img src = "images/jazz.png" class="image7"></a>
+				<!--Div for overlay techniques so that when the user hover it hide the images and display text -->
+				<div class="overlay">
+    				<div class="text"><a href = "Query4.php">Jazz</a></div>
+  				</div>
 			</div>
 			<div class="images8">
-				<a href = "#"><img src = "images/img.png"></a>
+				<a href = "Query5.php"><img src = "images/dance.jpg" class="image8"></a>
+				<!--Div for overlay techniques so that when the user hover it hide the images and display text -->
+				<div class="overlay">
+    				<div class="text"><a href = "Query5.php">Dance</a></div>
+  				</div>
 			</div>
 		</div>
+		<!--Footer for the music database -->
 		<div class = "footer">&copy; Copyright Tony Nguyen 2021</div>
 	</div>
 </body>
