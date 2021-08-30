0c0b8be6d175bf513da9bd17c469b43f94255de1
7tnguyen1 <86210235+7tnguyen1@users.noreply.github.com>
2021-08-30 12:06:21 +1200

    Create homepage.php

---
+++ homepage.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<title> Homepage</title>
+		<meta chrset="utf-8"/>
+		<meta name="Keywords" content = "html5"/>
+		<meta name = "Author" content = "Tony Nguyen"/>
+		<meta name = "Description" content = "Music database"/>
+		<meta name = "viewport" content = "width=device-width. initial-scale=1"/>
+		<!--Connected to css -->
+		<link rel ="stylesheet" href = "style6.css"> 
+		
+	</head>
+<body>
+	<div class ="grid-container">
+		<div class="img_1">
+			<img src = "images/banner.jpg">
+		</div>
+		<div class="navigation">
+			<div class="logo">
+				<a href = "homepage.php"><img src = "images/logo8.png" height = "100"></a>
+			</div>
+			<nav>
+				<ul>
+					<li><a href = "homepage.php">Home</a></li>
+					<li><a href = "musicplayer.php">Music Play</a>
+						<ul>
+							<li><a href= "Query1.php">Query1</a></li>
+							<li><a href= "Query2.php">Query2</a></li>
+						</ul>
+					</li>
+					<li><a href = "login.php">Contact</a></li>
+					<li><a href = "login.php">LoginOut</a></li>
+					<li><a href = "signup.php">SignUp</a></li>
+				</ul>
+			</nav>
+		</div>
+		<div class="content_1"><!-- Holds the Welcome and introduction about the page -->
+			<h1>Welcome to MUSIC MP3</h1>
+			<p>MUSIC MP3 is a free music player application. With a high-quality music store, you will have a great music experience.</p>
+			<p>Digital music service with millions of high quality copyrighted songs , helping you listen to music, download music, upload and sync my music store on multiple devices.</p>
+		</div>
+		<div class="content_2"><!-- Holds the About Us information -->
+			<h1>About Us</h1>
+			<p>MUSIC MP3 is a free music player application. With a high-quality music store, you will have a great music experience.</p>
+			<p>Digital music service with millions of high quality copyrighted songs , helping you listen to music, download music, upload and sync my music store on multiple devices.</p>
+		</div>
+		<div class = "footer">&copy; Copyright Tony Nguyen 2021</div>
+	</div>
+</body>
+</html>
