c6da1054d879d5ac471c84b040777d5fc1c7de59
7tnguyen1 <86210235+7tnguyen1@users.noreply.github.com>
2021-09-02 09:37:17 +1200

    Update Song_sort_Title.php

--- Song_sort_Title.php
+++ Song_sort_Title.php
@@ -1,4 +1,5 @@
 <?php
+		// Deliver to login page if access to this page with out login
 		session_start();
 		if(!isset($_SESSION['login_user'])){
 				header("location:login.php");
@@ -12,45 +13,43 @@
 <html lang = "en">
 	<head>
 		<title>Sort for query1</title>
-		<meta chrset="utf-8"/>
-		<meta name="Keywords" content = "html5, layout, CSS Grid System"/>
+		<meta name="Keywords" content = "html5"/>
 		<meta name = "Author" content = "Tony Nguyen"/>
-		<meta name = "Description" content = "CSS Grid System Layout Tutorial"/>
+		<meta name = "Description" content = "Music database"/>
 		<meta name = "viewport" content = "width=device-width. initial-scale=1"/>
-		
-		<link rel ="stylesheet" href = "Query1_V1.css"> 
+		<!--Connected to css -->
+		<link rel ="stylesheet" href = "style2.css"> 
 		
 	</head>
 <body>
+	<!--Set for grid system for the page -->
 	<div class ="grid-container">
+		<!-- The div for nav and in the div it contain the logo and a function to called the nav -->
 		<div class="navigation">
+			<!-- Set a div that contain logo for the music database -->
 			<div class="logo">
-				<a href = "homepage.php"><img src = "images/logo8.png" height = "100"></a>
+				<a href = "#"><img src = "images/logo8.png" height = "100"></a>
 			</div>
 			<?php
-				//Pulls the links from the nav.php page and places them in the navigation div
-				require 'nav_normal.css'; //'require' is 100% needed for the site to run 
+				//Pulls the links from the nav_admin.php page and places them in the navigation div
+				require 'nav_admin.php'; //'require' is 100% needed for the site to run 
 			?>
 		</div>
+		<!-- Div that holds the banner -->
 		<div class="img_1">
 			<img src = "images/banner.jpg">
 		</div>
 		<div class="content_2">
-			<img src = "images/img.png">
+			<img src = "images/ab.jpg">
 		</div>
-		<div class="content_1"><!-- Holds the main page content -->
-			<div class="section3"><!-- Holds the main page connect -->
+		<div class="content_1"><!-- Hold the total duration -->
+			<div class="section3">
 				<?php
+				// Called up the query for it to display the total duration of all song.
 				require "musicdatabase_database_mysqli.php";
 					
 				$query = ("SELECT SEC_TO_TIME(SUM(s.`Duration`)) AS 'Total time'
-						FROM `Song` AS s
-						JOIN Album AS al ON al.`Album_ID` = s.`Album_ID`
-						JOIN Song_to_Genre AS stg ON stg.`Song_ID` = s.`Song_ID`
-						JOIN Genre AS g ON g.`Genre_ID` = stg.`Genre_ID`
-						JOIN Song_to_Artist AS sta ON sta.`Song_ID` = s.`Song_ID`
-						JOIN Artist AS a ON a.`Artist_ID` = sta.`Artist_ID`
-						ORDER BY s.`Title` DESC, a.`Artist` DESC");
+				FROM `Song` AS s");
 				$result = mysqli_query($conn,$query);
 					
 				while($output = mysqli_fetch_array($result))
@@ -63,6 +62,7 @@
 				?>
 			</div>
 		</div>
+		<!-- Column that contain the name of what is display -->
 		<div class="content_3">
 			<Space></Space>
 			<h3>Title</h3>
@@ -71,17 +71,24 @@
 			<h3>Genre</h3>
 			<h3>Duration</h3>
 			<h3>Size</h3>				
+		</div>
+		<div class="content_4">
 			<?php
+			//Called up all data to display in the page and information of each column is place underneath their title
 				require "musicdatabase_database_mysqli.php";
 					
-				$query = ("SELECT s.`Title`, a.`Artist`, al.`Album`, g.`Genre`, s.`Duration`, s.`Size`
-						FROM `Song` AS s
-						JOIN Album AS al ON al.`Album_ID` = s.`Album_ID`
-						JOIN Song_to_Genre AS stg ON stg.`Song_ID` = s.`Song_ID`
-						JOIN Genre AS g ON g.`Genre_ID` = stg.`Genre_ID`
-						JOIN Song_to_Artist AS sta ON sta.`Song_ID` = s.`Song_ID`
-						JOIN Artist AS a ON a.`Artist_ID` = sta.`Artist_ID`
-						ORDER BY s.`Title` DESC, a.`Artist` DESC");
+				$query = ("SELECT s.`Title`, GROUP_CONCAT(DISTINCT a.`Artist` 
+				ORDER BY a.`Artist` DESC SEPARATOR ', ') AS 'Artist', 
+				GROUP_CONCAT(DISTINCT g.`Genre` SEPARATOR ', ') AS 'Genre', 
+				GROUP_CONCAT(DISTINCT al.`Album` SEPARATOR ', ') AS 'Album', s.`Duration`, s.`Size`
+				FROM `Song` AS s
+				JOIN Album AS al ON al.`Album_ID` = s.`Album_ID`
+				JOIN Song_to_Genre AS stg ON stg.`Song_ID` = s.`Song_ID`
+				JOIN Genre AS g ON g.`Genre_ID` = stg.`Genre_ID`
+				JOIN Song_to_Artist AS sta ON sta.`Song_ID` = s.`Song_ID`
+				JOIN Artist AS a ON a.`Artist_ID` = sta.`Artist_ID`
+				GROUP BY s.Title, s.Duration, s.Size
+				ORDER BY s.`Title` DESC");
 				$result = mysqli_query($conn,$query);
 					
 				while($output = mysqli_fetch_array($result))
@@ -99,6 +106,7 @@
 				}
 				?>
 		</div>
+		<!--Footer for the music database -->
 		<div class = "footer">&copy; Copyright Tony Nguyen 2021</div>
 	</div>
 </body>
