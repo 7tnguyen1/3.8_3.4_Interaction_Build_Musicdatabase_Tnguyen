0e07e1a544a29394c114b396cd28cc2123080ff6
7tnguyen1 <86210235+7tnguyen1@users.noreply.github.com>
2021-09-02 10:54:08 +1200

    Create Song_sort_Genre.php

---
+++ Song_sort_Genre.php
@@ -0,0 +1,105 @@
+<?php
+		session_start();
+		if(!isset($_SESSION['login_user'])){
+				header("location:login.php");
+				}
+		else{
+			$User = $_SESSION['login_user'];
+		}
+?>
+
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<title>Sort for query2</title>
+		<meta chrset="utf-8"/>
+		<meta name="Keywords" content = "html5, layout, CSS Grid System"/>
+		<meta name = "Author" content = "Tony Nguyen"/>
+		<meta name = "Description" content = "CSS Grid System Layout Tutorial"/>
+		<meta name = "viewport" content = "width=device-width. initial-scale=1"/>
+		
+		<link rel ="stylesheet" href = "Query1_V2.css"> 
+		
+	</head>
+<body>
+	<div class ="grid-container">
+		<div class="navigation">
+			<div class="logo">
+				<a href = "homepage.php"><img src = "images/logo8.png" height = "100"></a>
+			</div>
+			<?php
+				//Pulls the links from the nav.php page and places them in the navigation div
+				require 'nav_normal.css'; //'require' is 100% needed for the site to run 
+			?>
+		</div>
+		<div class="img_1">
+			<img src = "images/banner.jpg">
+		</div>
+		<div class="content_2">
+			<img src = "images/img.png">
+		</div>
+		<div class="content_1"><!-- Holds the main page content -->
+			<div class="section3"><!-- Holds the main page connect -->
+				<?php
+				require "musicdatabase_database_mysqli.php";
+					
+				$query = ("SELECT SEC_TO_TIME(SUM(s.`Duration`)) AS 'Total time'
+					FROM `Song` AS s
+					JOIN Album AS al ON al.`Album_ID` = s.`Album_ID`
+					JOIN Song_to_Genre AS stg ON stg.`Song_ID` = s.`Song_ID`
+					JOIN Genre AS g ON g.`Genre_ID` = stg.`Genre_ID`
+					JOIN Song_to_Artist AS sta ON sta.`Song_ID` = s.`Song_ID`
+					JOIN Artist AS a ON a.`Artist_ID` = sta.`Artist_ID`
+					ORDER BY `g`.`Genre` ASC, a.`Artist` ASC");
+				$result = mysqli_query($conn,$query);
+					
+				while($output = mysqli_fetch_array($result))
+					{
+				?>
+				<h1>All the song upload in this playlist.Total duration of playlist:<?php echo $output['Total time'];?></h1>
+				<?php
+				// Closes the output while loop
+				}
+				?>
+			</div>
+		</div>
+		<div class="content_3">
+			<Space></Space>
+			<h3>Title</h3>
+			<h3>Album</h3>
+			<h3>Artist</h3>
+			<h3>Genre</h3>
+			<h3>Duration</h3>
+			<h3>Size</h3>				
+			<?php
+				require "musicdatabase_database_mysqli.php";
+					
+				$query = ("SELECT g.`Genre`, a.`Artist`, al.`Album`, s.`Title`, s.`Duration`, s.`Size`
+					FROM `Song` AS s
+					JOIN Album AS al ON al.`Album_ID` = s.`Album_ID`
+					JOIN Song_to_Genre AS stg ON stg.`Song_ID` = s.`Song_ID`
+					JOIN Genre AS g ON g.`Genre_ID` = stg.`Genre_ID`
+					JOIN Song_to_Artist AS sta ON sta.`Song_ID` = s.`Song_ID`
+					JOIN Artist AS a ON a.`Artist_ID` = sta.`Artist_ID`
+					ORDER BY `g`.`Genre` ASC, a.`Artist` ASC");
+				$result = mysqli_query($conn,$query);
+					
+				while($output = mysqli_fetch_array($result))
+					{
+				?>
+				<Space></Space>
+				<h3> <p class="white"><?php echo $output['Title'];?></p></h3>
+				<h3><p class="white"><?php echo $output['Album']; ?></p></h3>
+				<h3><p class="white"><?php echo $output['Artist']; ?></p></h3>
+				<h3><p class="white"><?php echo $output['Genre']; ?></p></h3>
+				<h3> <p class="white"><?php echo $output['Duration'];?></p></h3>
+				<h3> <p class="white"><?php echo $output['Size'];?></p></h3>
+				<?php
+				// Closes the output while loop
+				}
+				?>
+		</div>
+		<div class = "footer">&copy; Copyright Tony Nguyen 2021</div>
+	</div>
+</body>
+</html>
