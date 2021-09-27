b1d5d1ac77980d48b4bbe0ab91d6bf5897cbf4eb
7tnguyen1 <86210235+7tnguyen1@users.noreply.github.com>
2021-09-28 09:58:38 +1300

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -19,7 +19,7 @@
 		<meta name = "Description" content = "Music database"/>
 		<meta name = "viewport" content = "width=device-width. initial-scale=1"/>
 		<!--Connected to css -->
-		<link rel ="stylesheet" href = "style1.css">
+		<link rel ="stylesheet" href = "homepage.css">
 		
 	</head>
 <body>
