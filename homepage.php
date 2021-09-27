4712a38b54f7cf00572e00270e2c8662e59bc0bf
7tnguyen1 <86210235+7tnguyen1@users.noreply.github.com>
2021-09-28 09:27:11 +1300

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -38,7 +38,7 @@
 		</div>
 		<!-- Div that holds the banner -->
 		<div class="img_1">
-			<img src = "images/banner.jpg">
+			<img src = "images/music-1106439_1280.jpg">
 		</div>
 		<div class="content_1"><!-- Holds the main page content -->
 			<h1>Welcome to MUSIC MP3</h1>
@@ -49,28 +49,28 @@
 		<div class="content_4">
 			<Space></Space>
 			<div class="images1">
-				<a href = "Query8.php"><img src = "images/indie.jpg" class="image1"></a>
+				<a href = "Query8.php"><img src = "images/Indie2.jpg" class="image1"></a>
 				<!--Div for overlay techniques so that when the user hover it hide the images and display text -->
 				<div class="overlay">
     				<div class="text"><a href = "Query8.php">Indie</a></div>
   				</div>
 			</div>
 			<div class="images2">
-				<a href = "Query7.php"><img src = "images/R&B.jpg" class="image2"></a>
+				<a href = "Query7.php"><img src = "images/RandB2.jpg" class="image2"></a>
 				<!--Div for overlay techniques so that when the user hover it hide the images and display text -->
 				<div class="overlay">
     				<div class="text"><a href = "Query7.php">R and B</a></div>
   				</div>
 			</div>
 			<div class="images3">
-				<a href = "Query6.php"><img src = "images/pop.jpg" class="image3"></a>
+				<a href = "Query6.php"><img src = "images/Pop2.png" class="image3"></a>
 				<!--Div for overlay techniques so that when the user hover it hide the images and display text -->
 				<div class="overlay">
     				<div class="text"><a href = "Query6.php">Pop</a></div>
   				</div>
 			</div>
 			<div class="images4">
-				<a href = "Query10.php"><img src = "images/rock.png" class="image4"></a>
+				<a href = "Query10.php"><img src = "images/rock2.png" class="image4"></a>
 				<!--Div for overlay techniques so that when the user hover it hide the images and display text -->
 				<div class="overlay">
     				<div class="text"><a href = "Query10.php">Rock</a></div>
@@ -78,28 +78,28 @@
 			</div>
 			<Space></Space>
 			<div class="images5">
-				<a href = "Query9.php"><img src = "images/easylisten.jpg" class="image5"></a>
+				<a href = "Query9.php"><img src = "images/5.jpg" class="image5"></a>
 				<!--Div for overlay techniques so that when the user hover it hide the images and display text -->
 				<div class="overlay">
     				<div class="text"><a href = "Query9.php">Easy Listen</a></div>
   				</div>
 			</div>
 			<div class="images6">
-				<a href = "Query3.php"><img src = "images/country.png" class="image6"></a>
+				<a href = "Query3.php"><img src = "images/6.jpg" class="image6"></a>
 				<!--Div for overlay techniques so that when the user hover it hide the images and display text -->
 				<div class="overlay">
     				<div class="text"><a href = "Query3.php">Country</a></div>
   				</div>
 			</div>
 			<div class="images7">
-				<a href = "Query4.php"><img src = "images/jazz.png" class="image7"></a>
+				<a href = "Query4.php"><img src = "images/jazz2.jpg" class="image7"></a>
 				<!--Div for overlay techniques so that when the user hover it hide the images and display text -->
 				<div class="overlay">
     				<div class="text"><a href = "Query4.php">Jazz</a></div>
   				</div>
 			</div>
 			<div class="images8">
-				<a href = "Query5.php"><img src = "images/dance.jpg" class="image8"></a>
+				<a href = "Query5.php"><img src = "images/dance muisc3.png" class="image8"></a>
 				<!--Div for overlay techniques so that when the user hover it hide the images and display text -->
 				<div class="overlay">
     				<div class="text"><a href = "Query5.php">Dance</a></div>
