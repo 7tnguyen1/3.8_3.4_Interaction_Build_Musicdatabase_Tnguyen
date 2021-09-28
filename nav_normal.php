5108d777c5d54aed45b7bd39084a45ddc4178d17
7tnguyen1 <86210235+7tnguyen1@users.noreply.github.com>
2021-09-28 13:50:53 +1300

    Create nav_normal.php

---
+++ nav_normal.php
@@ -0,0 +1,14 @@
+nav_admin.php
+<!--nav for User page -->
+<nav>
+	<ul>
+		<li><a href = "login.php">LoginOut</a></li>
+		<li><a href = "musicplayer.php">Music Play</a>
+			<ul>
+				<li><a href= "Song_sort_Title.php">Title</a></li>
+				<li><a href= "Song_sort_Genre.php">Genre</a></li>
+			</ul>
+		</li>
+		<li><a href = "homepage.php">Home</a></li>
+	</ul>
+</nav>
